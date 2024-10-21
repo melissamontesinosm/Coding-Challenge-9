323ac3e5e2f015ba8755932faf0b44374e4d0a12
Melissa Montesinos <melissa.d.montesinos@gmail.com>
2024-10-20 21:24:15 -0400

    Create Patron class to borrow and return books

--- library_management.js
+++ library_management.js
@@ -44,9 +44,41 @@ class Section { // Define a class
         return this.books.filter(book => book.isAvailable).length;
     };
 
-    listBooks() {
-        return this.books.map(book => `${book.title} - ${book.isAvailable} ? 'Available' : 'Borrowed'}`).join('\n');
-    }
-}
+    listBooks() { // List all books in the section with title and availability
+        return this.books.map(book => `${book.title} - ${book.isAvailable} ? 'Available' : 'Borrowed'}`).join('\n'); // Using a ternary operator to identify availability
+    };
+};
+
+// Task 3: Create a Patron Class
+
+class Patron {
+    constructor(name) {
+        this.name = name;
+        this.borrowedBooks = [];
+    };
+
+    borrowBook(book) { // To allow the patron to borrow a book if available and update book’s status.
+        if (book.isAvailable) { // If statement to display message
+            book.isAvailable = false; 
+            this.borrowedBooks.push(book); // Push borrowed book 
+            console.log(`${this.name} borrowed ${book.title}`); // Display message
+        } else {
+            console.log(`${book.title} is not available currently.`); // If else display message
+        };
+    };
+
+    returnBook(book) { // To allow the patron to return a borrowed book, making it available again.
+        const index = this.borrowedBooks.indexOf(book);
+        if(index !== -1) {
+            book.isAvailable = true; 
+            this.borrowedBooks.splice(index,1);
+            console.log(`${this.name} returned "${book.title}"`); // Display Message
+        } else {
+            console.log(`${this.name} does not have "${book.title}" borrowed.`); // If else display message
+        };
+    };
+};
+
+
 
 
